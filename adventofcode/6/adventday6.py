874e2ccba0ee5cb36a34a17598d81e0bb35804f2
amberlu00 <amberlu00@gmail.com>
2018-11-13 01:51:22 -0600

    Finished Day 6

--- adventofcode/6/adventday6.py
+++ adventofcode/6/adventday6.py
@@ -1,10 +1,23 @@
-f = open("adventday6text.txt")
-blocks = []
-correct_total = 0
-for entry in f.readline().split():
-	blocks.append(int(entry))
-	correct_total += int(entry)
-correct_total /= len(blocks)
+import copy
+# (4074, 2793)
 
-while b in blocks != correct_total:
-	print True
+f = open("adventday6text.txt")
+
+blocks = []
+block = []
+for entry in f.readline().split():
+    block.append(int(entry))
+
+while block not in blocks:
+    blocks.append(block[:])
+    max_num = max(block)
+    max_index = block.index(max_num)
+    i = block.index(max_num)
+    block[max_index] = 0
+    while max_num:
+        i = (i + 1) % len(block)
+        block[i] += 1
+        max_num -= 1
+
+print len(blocks)
+print len(blocks) - blocks.index(block)
